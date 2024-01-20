03adb4165e584eb89ecd1b15582ade039f8f9e2b
Aman Kushwaha <aman.kushwaha.inbox@gmail.com>
2024-01-20 17:46:08 +0530

    initial commit

--- android/app/src/main/java/io/wiry/vortexapp/MainActivity.kt
+++ android/app/src/main/java/io/wiry/vortexapp/MainActivity.kt
@@ -1,4 +1,6 @@
 package io.wiry.vortexapp
+// added for react-native-screens
+import android.os.Bundle
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
@@ -7,6 +9,10 @@ import com.facebook.react.defaults.DefaultReactActivityDelegate
 
 class MainActivity : ReactActivity() {
 
+  // added for react-native-screens
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(null)
+  }
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
---
+++ src/screens/HistoryScreen.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import {View} from 'react-native';
+import {Text} from 'react-native-paper';
+
+export default function HistoryScreen() {
+  return (
+    <View>
+      <Text>History Screen</Text>
+    </View>
+  );
+}
